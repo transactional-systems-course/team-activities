b9864f54973a8b34edd5b96a41e6b7e6400f9b9b
Jaime Andres Torres B <69700780+XaurDesu@users.noreply.github.com>
2022-10-02 16:18:11 -0500

    Feat: Add query-2, query_3

--- talleres/taller2-sql/query-2.sql
+++ talleres/taller2-sql/query-2.sql
@@ -1,1 +1,29 @@
 ALTER SESSION SET CURRENT_SCHEMA = PARRANDEROS;
+SELECT NOMBRETABLA, NOMBRECOLSPK, TIPODEDATO FROM
+(
+    (
+        SELECT 
+            ALL_CONS_COLUMNS.TABLE_NAME AS NOMBRETABLA,
+            ALL_CONS_COLUMNS.COLUMN_NAME AS NOMBRECOLSPK
+        FROM 
+            ALL_CONSTRAINTS, ALL_CONS_COLUMNS
+        WHERE
+            all_constraints.constraint_type = 'P'
+            and all_constraints.constraint_name = all_cons_columns.constraint_name
+            and all_constraints.owner = all_cons_columns.owner
+            AND all_cons_columns.owner = 'PARRANDEROS'
+    ) PKS
+    INNER JOIN
+    (
+        SELECT
+            TABLE_NAME, COLUMN_NAME, DATA_TYPE AS TIPODEDATO
+        FROM
+            ALL_TAB_COLUMNS
+        WHERE 
+            OWNER = 'PARRANDEROS'
+    ) DTYPE
+    ON PKS.NOMBRECOLSPK = DTYPE.COLUMN_NAME
+    AND PKS.NOMBRETABLA = DTYPE.TABLE_NAME
+) ORDER BY NOMBRETABLA;
+    
+    
