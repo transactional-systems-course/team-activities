230c243a1747c02fc602a3e43f03d6369f7fe53b
Juan Alegría <juanszalegria@gmail.com>
2022-10-17 12:52:35 -0500

    Fix: add new property to promocion

---
+++ proyecto/iteración-2/docs/sql/rfc/RFC6.sql
@@ -0,0 +1,8 @@
+-- Show sales of SuperAndes to a given customer, in a range of dates
+
+Esta operación es realizada por los gerentes de sucursal y por el gerente general del supermercado.
+Por razones de privacidad, un gerente de sucursal sólo puede ver la información de su sucursal, y de los
+clientes de su sucursal, mientras que el gerente general puede ver la información de todas las sucursales. 
+6
+También puede ser solicitada por el cliente. En ese caso, un cliente sólo puede ver su propia información.
+
