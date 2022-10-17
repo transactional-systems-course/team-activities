305c0ac5423ae4a188fe626b8c56e01c0909b110
Juan Alegría <juanszalegria@gmail.com>
2022-10-17 16:36:30 -0500

    Feat: add SQL constructors

--- proyecto/iteración-2/src/main/java/uniandes/isis2304/superandes/persistencia/SQLProductosCompra.java
+++ proyecto/iteración-2/src/main/java/uniandes/isis2304/superandes/persistencia/SQLProductosCompra.java
@@ -1,5 +1,53 @@
 package uniandes.isis2304.superandes.persistencia;
 
+import java.util.List;
+
+import javax.jdo.PersistenceManager;
+import javax.jdo.Query;
+
+import uniandes.isis2304.superandes.negocio.ProductosCompra;
+
+/**
+ * Clase que encapsula los métodos que hacen acceso a la base de datos para el
+ * concepto ProductosCompra de SuperAndes
+ * Nótese que es una clase que es sólo conocida en el paquete de persistencia
+ */
 public class SQLProductosCompra {
-    
+    /*
+     * ****************************************************************
+     * Constantes
+     *****************************************************************/
+    /**
+     * Cadena que representa el tipo de consulta que se va a realizar en las
+     * sentencias de acceso a la base de datos
+     * Se renombra acá para facilitar la escritura de las sentencias
+     */
+    private final static String SQL = PersistenciaSuperAndes.SQL;
+
+    /*
+     * ****************************************************************
+     * Atributos
+     *****************************************************************/
+    /**
+     * El manejador de persistencia general de la aplicación
+     */
+    private PersistenciaSuperAndes ps;
+
+    /*
+     * ****************************************************************
+     * Métodos
+     *****************************************************************/
+
+    /**
+     * Constructor
+     *
+     * @param ps - El Manejador de persistencia de la aplicación
+     */
+    public SQLProductosCompra(PersistenciaSuperAndes ps) {
+        this.ps = ps;
+    }
+
+    /**
+     * TODO: CRUD
+     */
 }
--- proyecto/iteración-2/src/main/java/uniandes/isis2304/superandes/persistencia/SQLRol.java
+++ proyecto/iteración-2/src/main/java/uniandes/isis2304/superandes/persistencia/SQLRol.java
@@ -1,5 +1,53 @@
 package uniandes.isis2304.superandes.persistencia;
 
+import java.util.List;
+
+import javax.jdo.PersistenceManager;
+import javax.jdo.Query;
+
+import uniandes.isis2304.superandes.negocio.Rol;
+
+/**
+ * Clase que encapsula los métodos que hacen acceso a la base de datos para el
+ * concepto Rol de SuperAndes
+ * Nótese que es una clase que es sólo conocida en el paquete de persistencia
+ */
 public class SQLRol {
-    
+    /*
+     * ****************************************************************
+     * Constantes
+     *****************************************************************/
+    /**
+     * Cadena que representa el tipo de consulta que se va a realizar en las
+     * sentencias de acceso a la base de datos
+     * Se renombra acá para facilitar la escritura de las sentencias
+     */
+    private final static String SQL = PersistenciaSuperAndes.SQL;
+
+    /*
+     * ****************************************************************
+     * Atributos
+     *****************************************************************/
+    /**
+     * El manejador de persistencia general de la aplicación
+     */
+    private PersistenciaSuperAndes ps;
+
+    /*
+     * ****************************************************************
+     * Métodos
+     *****************************************************************/
+
+    /**
+     * Constructor
+     *
+     * @param ps - El Manejador de persistencia de la aplicación
+     */
+    public SQLRol(PersistenciaSuperAndes ps) {
+        this.ps = ps;
+    }
+
+    /**
+     * TODO: CRUD
+     */
 }
