2976f30272f97d1809d0385cd71676c158c1522f
Juan Alegría <juanszalegria@gmail.com>
2022-12-04 18:31:50 -0500

    Style: format RFCs

--- proyecto/iteración-4/docs/sql/rfc/RFC11.sql
+++ proyecto/iteración-4/docs/sql/rfc/RFC11.sql
@@ -1,71 +1,44 @@
 CREATE VIEW COMPRAS_USUARIO AS
-    SELECT
-        *
-    FROM
-        ( (
-            SELECT
-                *
-            FROM
-                USUARIO
+    SELECT *
+    FROM ( (
+            SELECT *
+            FROM USUARIO
         ) FULL OUTER JOIN (
-            SELECT
-                *
-            FROM
-                COMPRA
+            SELECT *
+            FROM COMPRA
         ) ON NUMERO_DOCUMENTO = COMPRADOR FULL OUTER JOIN (
-            SELECT
-                *
-            FROM
-                PRODUCTOS_COMPRA
+            SELECT *
+            FROM PRODUCTOS_COMPRA
         ) ON ID_COMPRA = ID );
 
 CREATE VIEW USUARIOS_CON_COMPRA AS
-    SELECT
-        *
-    FROM
-        (
-            SELECT
-                *
-            FROM
-                ( (
-                    SELECT
-                        *
-                    FROM
-                        USUARIO
+    SELECT *
+    FROM (
+            SELECT *
+            FROM ( (
+                    SELECT *
+                    FROM USUARIO
                 ) INNER JOIN (
-                    SELECT
-                        *
-                    FROM
-                        COMPRA
+                    SELECT *
+                    FROM COMPRA
                 ) ON NUMERO_DOCUMENTO = COMPRADOR INNER JOIN (
-                    SELECT
-                        *
-                    FROM
-                        PRODUCTOS_COMPRA
+                    SELECT *
+                    FROM PRODUCTOS_COMPRA
                 ) ON ID_COMPRA = ID )
         );
 
-SELECT
-    *
-FROM
-    (
-        SELECT
-            *
-        FROM
-            COMPRAS_USUARIO
-        WHERE
-            NUMERO_DOCUMENTO NOT IN (
-                SELECT
-                    NUMERO_DOCUMENTO
-                FROM
-                    USUARIOS_CON_COMPRA
-                WHERE
-                    FECHA_COMPRA BETWEEN '01-JAN-2020'
-                    AND '01-JAN-2022'
-                    AND ESTADO_COMPRA = 'PAGADA'
+SELECT *
+FROM (
+        SELECT *
+        FROM COMPRAS_USUARIO
+        WHERE NUMERO_DOCUMENTO NOT IN (
+                SELECT NUMERO_DOCUMENTO
+                FROM USUARIOS_CON_COMPRA
+                WHERE FECHA_COMPRA BETWEEN '01-JAN-2020' AND
+                    '01-JAN-2022' AND
+                    ESTADO_COMPRA = 'PAGADA'
             )
     );
-
 
 /*
 SELECT
@@ -90,7 +63,5 @@ FROM
     );
 */
 
-SELECT
-    *
-FROM
-    USUARIOS_CON_COMPRA
+SELECT *
+FROM USUARIOS_CON_COMPRA
